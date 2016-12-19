a0265fdc632ebc55df892f461ef0a523bd8cb8d4
Thomas Chen <tkchen@gmail.com>
2016-12-18 18:01:41 -0800

    Fix compile.

--- post-build.sh
+++ post-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=$1
 FILE=$2
-DEST="/Applications/Quantum.app/Contents/Resources/.kivy/extensions/plugins/"
+DEST="/Applications/Quantum.app/Contents/Resources/.kivy/extensions/plugins/tests/"
 cp ${DIR}/${FILE} ${DEST}
 install_name_tool -id ./Contents/Resources/.kivy/extensions/plugins/${FILE} ${DEST}${FILE}
 #install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.5/Python @executable_path/../Frameworks/python/3.5.0/Python ${DEST}${FILE}
