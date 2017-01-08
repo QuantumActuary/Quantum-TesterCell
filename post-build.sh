12461f97aa654e6224809887610f8ac7292a9f3d
Thomas Chen <tkchen@gmail.com>
2017-01-08 14:27:49 -0800

    Update for python 3.6 and fix installation

--- post-build.sh
+++ post-build.sh
@@ -2,7 +2,11 @@
 DIR=$1
 FILE=$2
 DEST="/Applications/Quantum.app/Contents/Resources/.kivy/extensions/plugins/tests/"
+if [ ! -d $DEST ]; then
+    mkdir $DEST;
+fi;
+osxrelocator -r . ./Contents @executable_path/..
 cp ${DIR}/${FILE} ${DEST}
-install_name_tool -id ./Contents/Resources/.kivy/extensions/plugins/${FILE} ${DEST}${FILE}
+#install_name_tool -id ./Contents/Resources/.kivy/extensions/plugins/${FILE} ${DEST}${FILE}
 #install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.5/Python @executable_path/../Frameworks/python/3.5.0/Python ${DEST}${FILE}
 #install_name_tool -change /usr/lib/libc++.1.dylib @executable_path/lib/libc++.1.dylib ${DEST}${FILE}
